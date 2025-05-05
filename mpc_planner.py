8812f6eeaad0d3ca824aaf2dd5716b08108ebee0
Iryna Hurova <iryna.gurova@gmail.com>
2025-05-05 22:28:06 +0300

    added mpc demo with collision avoidance and goal reaching

--- mpc_planner.py
+++ mpc_planner.py
@@ -1,10 +1,7 @@
 import numpy as np
 from cem_optimization import cem_optimization
-import mujoco.mjx as mjx 
 import mujoco
 import time
-import jax.numpy as jnp
-import jax
 import os
 from mujoco import viewer
 import matplotlib.pyplot as plt
@@ -35,16 +32,16 @@ class MPC_Planner():
 
     def init_cem(self):
         start_time = time.time()
-        self.cem =  cem_optimization(num_dof=6, num_batch=500, num_steps=8, maxiter_cem=1,
-                           w_pos=1, w_rot=0.5, w_col=10, num_elite=0.05, timestep=0.05)
+        self.cem =  cem_optimization(num_dof=6, num_batch=1000, num_steps=8, maxiter_cem=1,
+                           w_pos=5, w_rot=1.5, w_col=5, num_elite=0.05, timestep=0.05)
         print(f"Initialized CEM Planner: {round(time.time()-start_time, 2)}s")
 
         self.model = self.cem.model
         self.data = self.cem.data
-        self.data.qpos[:6] = jnp.array(self.init_joint_position)
+        self.data.qpos[:6] = np.array(self.init_joint_position)
         mujoco.mj_forward(self.model, self.data)
 
-        self.xi_mean = jnp.zeros(self.cem.nvar)
+        self.xi_mean = np.zeros(self.cem.nvar)
         self.target_pos = self.model.body(name="target_0").pos
         self.target_rot = self.model.body(name="target_0").quat
 
@@ -63,7 +60,25 @@ class MPC_Planner():
                                       init_pos=current_pos, init_vel=current_vel, 
                                       target_pos=self.target_pos, target_rot=self.target_rot)
         print(f"Compute CEM: {round(time.time()-start_time, 2)}s")
-        
+
+        init_position = self.data.site_xpos[self.model.site(name="tcp").id].copy()
+        init_rotation = self.data.xquat[self.model.body(name="hande").id].copy()
+
+        target_positions = [
+            [-0.3, 0.3, 0.8],
+            [-0.2, -0.4, 1.0],
+            [-0.3, -0.1, 0.8],
+            init_position
+        ]
+
+        target_rotations = [
+            rotation_quaternion(-135, np.array([1,0,0])),
+            quaternion_multiply(rotation_quaternion(90, np.array([0,0,1])),rotation_quaternion(135, np.array([1,0,0]))),
+            quaternion_multiply(rotation_quaternion(180, np.array([0,0,1])),rotation_quaternion(-90, np.array([0,1,0]))),
+            init_rotation
+        ]
+
+        target_idx = 0        
         with viewer.launch_passive(self.model, self.data) as viewer_:
             viewer_.cam.distance = 4
             viewer_.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
@@ -102,6 +117,12 @@ class MPC_Planner():
                 time_until_next_step = self.model.opt.timestep - (time.time() - start_time)
                 if time_until_next_step > 0:
                     time.sleep(time_until_next_step) 
+
+                if cost_g<0.01 and cost_r<0.3:
+                    self.model.body(name="target_0").pos = target_positions[target_idx]
+                    self.model.body(name="target_0").quat = target_rotations[target_idx]
+                    if target_idx<len(target_positions)-1:
+                        target_idx += 1
                 
                 print(f'Step Time: {"%.0f"%((time.time() - start_time)*1000)}ms | Cost g: {"%.2f"%(float(cost_g))} | Cost r: {"%.2f"%(float(cost_r))} | Cost c: {"%.2f"%(float(cost_c))} | Cost: {cost}')
 
