1adb5ed29e25ecb9e316be5ae0346ec0e5357f9d
alinjar1996 <alinjardannitdgp2014@gmail.com>
2025-06-24 11:51:02 +0300

    Issue #4

--- trajectory_sampler2.py
+++ trajectory_sampler2.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import jax
 import jax.numpy as jnp
 
-jax.config.update("jax_enable_x64", True)
+# jax.config.update("jax_enable_x64", True)
 
 
 class TrajSampler():
